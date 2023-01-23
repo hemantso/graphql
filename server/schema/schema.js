e058b50255312bf94e4120d7d662c40f52601506
Hemant soni <sonihemant4741@gmail.com>
2023-01-23 16:01:13 +0530

    Author schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,23 @@
 const graphql = require('graphql');
 const _ = require("lodash")
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } = graphql;
 
 
 const BookType = new GraphQLObjectType({
   name:'Book',
   fields: () => ({
-    id: { type: GraphQLString},
+    id: { type: GraphQLID},
     name: {type: GraphQLString},
     genre: { type: GraphQLString}
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name:'Author',
+  fields: () => ({
+    id: { type: GraphQLID},
+    name: {type: GraphQLString},
+    age: { type: GraphQLInt }
   })
 });
 
@@ -23,6 +32,13 @@ const RootQuery = new GraphQLSObjectType({
       resolve(parent, args) {
         // code to get data from db 
         return _.find(books, {id: args.id});
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args){
+        return _.find(authors, {id: args.id});
       }
     }
   }
